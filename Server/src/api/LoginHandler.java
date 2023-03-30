ec6edae18588e89b2f212a9cc7f2f7b0a5addcab
maitanegarcia <maitane.garcia02@opendeusto.es>
2023-03-31 00:04:49 +0200

    Create the loginhandler on the serverside

--- Server/src/api/LoginHandler.java
+++ Server/src/api/LoginHandler.java
@@ -1,13 +1,25 @@
 package api;
 
 import java.io.IOException;
+import java.io.OutputStream;
+
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
 public class LoginHandler  implements HttpHandler{
 
+private String message;
+	
+	
 	@Override
     public void handle(HttpExchange t) throws IOException {
+		String response = message;
+		t.sendResponseHeaders(200, response.length());
+		OutputStream os = t.getResponseBody();
+		os.write(response.getBytes());
+		os.close();
+
+		 
     }
 
 }
