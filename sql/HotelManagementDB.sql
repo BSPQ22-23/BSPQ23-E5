b0559c63886e2f8c2689be0206fb94f143eeb3cc
maitanegarcia <maitane.garcia02@opendeusto.es>
2023-05-01 23:22:19 +0200

    Update Database

--- sql/HotelManagementDB.sql
+++ sql/HotelManagementDB.sql
@@ -1,5 +1,5 @@
 
-/* DELETE 'strava_user' database*/
+/* DELETE 'HotelManagementDB' database*/
 DROP SCHEMA IF EXISTS HotelManagementDB;
 /* DELETE USER 'strava_user' AT LOCAL SERVER*/
 DROP USER IF EXISTS 'spq'@'localhost';
@@ -8,5 +8,5 @@ DROP USER IF EXISTS 'spq'@'localhost';
 CREATE SCHEMA IF NOT EXISTS HotelManagementDB;
 /* CREATE THE USER 'strava_user' AT LOCAL SERVER WITH PASSWORD 'strava_user' */
 CREATE USER IF NOT EXISTS 'spq'@'localhost' IDENTIFIED BY 'spq';
-/* GRANT FULL ACCESS TO THE DATABASE 'stravadb' FOR THE USER 'strava_user' AT LOCAL SERVER*/
-GRANT ALL ON strava.* TO 'sd'@'localhost';
+/* GRANT FULL ACCESS TO THE DATABASE 'HotelManagementDB' FOR THE USER 'strava_user' AT LOCAL SERVER*/
+GRANT ALL ON  HotelManagementDB.* TO 'sd'@'localhost';
