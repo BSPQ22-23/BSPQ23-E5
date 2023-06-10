efcccb531a47cb23e814ab9372b80bd2503ab587
maitanegarcia <maitane.garcia02@opendeusto.es>
2023-06-10 20:30:51 +0200

    Reservation functionality

--- sql/HotelManagementDB.sql
+++ sql/HotelManagementDB.sql
@@ -1,7 +1,12 @@
-DROP SCHEMA IF EXISTS HotelManagement;
+
+/* DELETE 'HotelManagementDB' database*/
+DROP SCHEMA IF EXISTS HotelManagerDB;
+/* DELETE USER 'strava_user' AT LOCAL SERVER*/
 DROP USER IF EXISTS 'spq'@'localhost';
 
-CREATE SCHEMA HotelManagement;
+/* CREATE 'stravadb' DATABASE */
+CREATE SCHEMA IF NOT EXISTS HotelManagerDB;
+/* CREATE THE USER 'strava_user' AT LOCAL SERVER WITH PASSWORD 'strava_user' */
 CREATE USER IF NOT EXISTS 'spq'@'localhost' IDENTIFIED BY 'spq';
-
-GRANT ALL ON HotelManagement.* TO 'spq'@'localhost';
+/* GRANT FULL ACCESS TO THE DATABASE 'HotelManagementDB' FOR THE USER 'strava_user' AT LOCAL SERVER*/
+GRANT ALL ON  HotelManagerDB.* TO 'spq'@'localhost';
---
+++ src/test/java/es/deusto/spq/pojo/BookingDataTest.java
@@ -0,0 +1,24 @@
+package es.deusto.spq.pojo;
+
+import static org.junit.Assert.*;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class BookingDataTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
